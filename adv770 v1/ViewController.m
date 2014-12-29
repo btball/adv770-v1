4d32bab903467e9076d1c4bff34c9e9dc1b7cbb8
Brian Ball <btball@gmail.com>
2014-12-29 11:09:52 -0800

    Initial Commit

---
+++ adv770 v1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  adv770 v1
+//
+//  Created by Brian Ball on 12/29/14.
+//  Copyright (c) 2014 Brian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
