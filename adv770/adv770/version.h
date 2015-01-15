b94ca39cebf11076ea10848eb54d97e064c83e2c
Brian Ball <btball@gmail.com>
2015-01-14 19:18:16 -0800

    more fun with 770

---
+++ adv770/adv770/version.h
@@ -0,0 +1,12 @@
+//
+//  version.h
+//  adv770
+//
+//  Created by Brian Ball on 1/14/15.
+//  Copyright (c) 2015 Brian. All rights reserved.
+//
+
+#ifndef adv770_version_h
+#define adv770_version_h
+char * adv_version = "_START_HTML_adv770";
+#endif
